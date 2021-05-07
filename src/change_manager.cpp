e3ae04d7ea83a5c9c9f9a52084b64e7de6437bd3
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-08 01:51:19 +0530

    change_manager.cpp: define `register_`, `unregister_`, and `notify_`
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/change_manager.cpp
+++ src/change_manager.cpp
@@ -10,9 +10,41 @@ ChangeManager* ChangeManager::get()
 	return instance_;
 }
 
+
 void ChangeManager::remove()
 {
 	if (instance_)
 		delete instance_;
 
+}
+
+
+void ChangeManager::register_(Subject* s, Observer* o)
+{
+	subject_observer.insert(std::pair<Subject *, Observer*>(s, o));
+}
+
+
+void ChangeManager::unregister_(Subject* s, Observer* o)
+{
+	std::pair<iterator, iterator> iterpair = subject_observer.equal_range(s);
+	iterator it = iterpair.first;
+
+	for (; it != iterpair.second; ++it) {
+		if (it->second == o) { 
+			subject_observer.erase(it);
+			break;
+		}
+	}
+}
+
+
+void ChangeManager::notify_(Subject* s)
+{
+	std::pair<iterator, iterator> iterpair = subject_observer.equal_range(s);
+	iterator it = iterpair.first;
+
+	for (; it != iterpair.second; ++it) {
+		it->second->update(s);
+	}
 }
