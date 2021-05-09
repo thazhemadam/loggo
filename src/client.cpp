e3c5d8bff1437407fd6157dbf96cf92c004065e8
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-10 03:23:18 +0530

    client.cpp: update example in client.cpp
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/client.cpp
+++ src/client.cpp
@@ -1,31 +1,32 @@
 #include "logga.h"
+using namespace std;
 
 int main()
 {
-	Library observer1("STATE - 1");
-	Library observer2("STATE - 2");
+	Library library_1("LIB - 1");
+	Library library_2("LIB - 2");
+	Library library_3("LIB - 3");
+	Library library_4("LIB - 4");
+	Library library_5("LIB - 5");
+	Library library_6("LIB - 6");
 
-	observer1.disp();
-	observer2.disp();
-
-	Library subject;
-	subject.attach(&observer1);
-	subject.attach(&observer2);
-
-	subject.set_state("STATE - LIQUID");
-	subject.notify();
-
-	observer1.disp();
-	observer2.disp();
+	library_1.attach(&library_4);
+	library_2.attach(&library_4);
+	library_2.attach(&library_5);
+	library_3.attach(&library_5);
+	library_4.attach(&library_5);
+	library_5.attach(&library_6);
 	
-
-	subject.detach(&observer1);
-
-	subject.set_state("STATE - Karnataka");
-	subject.notify();
-
-	observer1.disp();
-	observer2.disp();
+	cout << "-------------------------------------\n";
+	library_2.set_state("STATE - LIQUID", true);
+	cout << "-------------------------------------\n";
+	library_1.disp();
+	library_2.disp();
+	library_3.disp();
+	library_4.disp();
+	library_5.disp();
+	library_6.disp();
+	cout << "-------------------------------------\n";
 
 	return 0;
 }
